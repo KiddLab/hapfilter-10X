ac7877c1b739e049ca0397b2186e50b3741a3255
Jeff Kidd <jmkidd@med.umich.edu>
2018-04-13 16:10:59 -0400

    works for handfull of sites, minimum filters

---
+++ hapfilt.py
@@ -0,0 +1,52 @@
+#hapfilt.py
+import sys
+import pysam
+#######################################################################
+def count_site(bamFileName,chrom,pos,ref,alt):
+   # counts will be a list of list for
+   # hap1, hap2, haplotype not assigned
+   # and then for each haplotype
+   # ref counts, alt counts, other allele, alt/(ref+alt)
+
+    counts = [[0,0,0,0.0],[0,0,0,0.0],[0,0,0,0.0]]
+    samfile = pysam.AlignmentFile(bamFileName, 'rb')
+    # get  reads overlapping segment
+    for read in samfile.fetch(chrom,pos-1,pos):
+        # some simple filters, not being very strict at all...
+        if read.is_duplicate is True:
+            continue
+        if read.is_qcfail is True:
+            continue    
+        if read.mapping_quality <= 10:
+            continue    
+        
+        aligned_pairs = read.get_aligned_pairs(with_seq=False) 
+        queryAlignedBase = '?' # incase we don't have an aligned base, probably not needed
+        numAligned = 0
+        for i in aligned_pairs:
+            if i[1] == pos-1:
+                queryAlignedBase = read.query_sequence[i[0]]
+                numAligned += 1
+        if numAligned != 1:  #might see multiple aligned in case of indel
+            continue
+        if queryAlignedBase == '?':  # no alignment...
+            continue
+        if queryAlignedBase == ref:
+            allele_i = 0
+        elif queryAlignedBase == alt:
+            allele_i = 1
+        else:
+            allele_i = 2
+        if read.has_tag('HP'):
+            hapTag = read.get_tag('HP')
+            hap_num = hapTag - 1
+        else:
+            hap_num = 2 # no hap
+        counts[hap_num][allele_i] += 1
+    samfile.close()
+    
+    for i in range(3): # get the alt read fraction
+        t = counts[i][0]+counts[i][1]
+        counts[i][3] = float(counts[i][1])/t    
+    return counts
+#######################################################################
---
+++ test-tools.py
@@ -0,0 +1,63 @@
+import sys
+import hapfilt
+
+bamFileName = '/home/jmkidd/links/dcmb-brainsom/technical/working/20170601_10x_bam_longranger/brain.bam'
+
+#SNVval1	1	228475850	.	A	G	chr1:228475791-228476023
+chrom = '1'
+pos = 228475850
+ref = 'A'
+alt = 'G'
+
+
+chrom = '19'
+pos = 4510979
+ref = 'A'
+alt = 'C'
+
+
+#SNVval7	6	163235167	.	T
+chrom = '6'
+pos = 163235167
+ref = 'T'
+alt = 'C'
+#counts = hapfilt.count_site(bamFileName,chrom,pos,ref,alt)
+#print counts
+
+#nl = [chrom,pos,ref,alt]
+#for i in counts:
+#    nl.extend(i)    
+#nl = [str(j) for j in nl]    
+#print nl
+
+
+
+chrom = '16'
+pos = 30795009
+ref = 'A'
+alt = 'T'
+counts = hapfilt.count_site(bamFileName,chrom,pos,ref,alt)
+print counts
+
+nl = [chrom,pos,ref,alt]
+for i in counts:
+    nl.extend(i)    
+nl = [str(j) for j in nl]    
+print nl
+
+
+
+
+print 'next'
+chrom = '1'
+pos = 228475850
+ref = 'A'
+alt = 'G'
+counts = hapfilt.count_site(bamFileName,chrom,pos,ref,alt)
+print counts
+
+nl = [chrom,pos,ref,alt]
+for i in counts:
+    nl.extend(i)    
+nl = [str(j) for j in nl]    
+print nl
